ccbca77defd5c5944ee4048e5dfc22aeb302cef2
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2022-11-20 15:09:10 -0800

    Adding video models and BE config

--- App/rosen_backend/videoUpload/views.py
+++ App/rosen_backend/videoUpload/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import VideoUpload
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request,"index.html")
+    vid = VideoUpload.objects.all()
+    return render(request,"index.html",{"video":vid})
