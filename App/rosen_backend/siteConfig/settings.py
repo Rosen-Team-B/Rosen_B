6d1de0de9c025d4cccbbe7299a047108b4270cf0
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2022-11-20 13:22:28 -0800

    Resetting default DB

--- App/rosen_backend/siteConfig/settings.py
+++ App/rosen_backend/siteConfig/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'videoUpload',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -75,18 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
