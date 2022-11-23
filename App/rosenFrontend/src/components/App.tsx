17f03174da1a7866c84b73d5cdbc74b1f63c30a2
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2022-11-22 22:23:01 -0800

    Update App.tsx

--- App/rosenFrontend/src/components/App.tsx
+++ App/rosenFrontend/src/components/App.tsx
@@ -4,11 +4,13 @@ import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
-
+import { Route } from "react-router-dom";
 function App() {
   return (
     // placeholder for future pages
     <Step2 />
+
+    // <Route render={() => <Step2 />} />
   );
 }
 
