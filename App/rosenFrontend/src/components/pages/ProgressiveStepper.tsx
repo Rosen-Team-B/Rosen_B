1ace1d7ac0591e75e04e86f25e66f8b43b9e072f
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2022-11-24 13:15:29 -0800

    stepper movement

--- App/rosenFrontend/src/components/pages/ProgressiveStepper.tsx
+++ App/rosenFrontend/src/components/pages/ProgressiveStepper.tsx
@@ -3,6 +3,7 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+import GeneralButton from "../shared/button/Button";
 
 const steps = ["Step 1", "Step 2", "Step 3", "Step 4"];
 
@@ -15,15 +16,29 @@ const ProgressiveStepper = (props: any): React.ReactElement => {
     setActiveStep(activeStep + 1);
   }
   return (
-    <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-    </Box>
+    <React.Fragment>
+      <React.Fragment>
+        <Box sx={{ width: "100%" }}>
+          <Stepper activeStep={activeStep} alternativeLabel>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
+      </React.Fragment>
+      <React.Fragment>
+        <GeneralButton
+          background="linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+          color="white"
+          onClick={() => nextButton()}
+          padding="0 30px"
+          height="40px"
+          children="Next"
+        />
+      </React.Fragment>
+    </React.Fragment>
   );
 };
 
