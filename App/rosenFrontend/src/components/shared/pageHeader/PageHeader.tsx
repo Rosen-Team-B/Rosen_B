8387e865e7242e487be5550ada87c6fdcd0fb7de
yuqi88 <snow34545@gmail.com>
2022-11-25 17:18:01 -0800

    add PageHeader styles

--- App/rosenFrontend/src/components/shared/pageHeader/PageHeader.tsx
+++ App/rosenFrontend/src/components/shared/pageHeader/PageHeader.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import logo from './Rosen_logo.png'
+// import Styles from './PageHeader.module.scss'
 
+//Will move the styles to a separate css file in the future.
+const styles = {
+    pageContainer: {
+        background: "#365578",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+
+    },
+    innerContainer:{
+        padding: "0.5rem 2rem",
+    },
+    logo:{
+        maxHeight: "2.5rem",
+    },
+    username:{
+        color: "white",
+        fontWeight: "bold",
+    },
+};
 
 const PageHeader = () => {
 
