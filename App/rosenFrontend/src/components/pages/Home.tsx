41bb61b655dd63208b28f9557748ee2671d33472
yuqi88 <snow34545@gmail.com>
2022-11-25 22:06:54 -0800

    add pageHeader to home page

--- App/rosenFrontend/src/components/pages/Home.tsx
+++ App/rosenFrontend/src/components/pages/Home.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import GeneralButton from "../shared/button/Button";
-import { RoutePaths } from "../../constants/routes";
+import {RoutePaths} from "../../constants/routes";
+import PageHeader from "../shared/pageHeader/PageHeader";
 
 function Home() {
-  return (
-    <>
-      <Link to={RoutePaths.Stepper}>
-        <GeneralButton
-          background="linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
-          color="white"
-          onClick={() => console.log("You clicked on the button")}
-          children="Stepper"
-          padding="0 30px"
-          height="40px"
-        />
-      </Link>
-    </>
-  );
+    return (
+        <>
+            <PageHeader/>
+            <Link to={RoutePaths.Stepper}>
+                <GeneralButton
+                    background="linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+                    color="white"
+                    onClick={() => console.log("You clicked on the button")}
+                    children="Stepper"
+                    padding="0 30px"
+                    height="40px"
+                />
+            </Link>
+        </>
+    );
 }
 
 export default Home;
