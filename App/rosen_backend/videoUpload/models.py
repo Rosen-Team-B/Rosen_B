be765e862431a680ce33768b30fedfd338c5dd64
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2022-12-27 15:34:33 -0800

    Changing video upload model

--- App/rosen_backend/videoUpload/models.py
+++ App/rosen_backend/videoUpload/models.py
@@ -2,7 +2,4 @@ from django.db import models
 
 # Create your models here.
 class VideoUpload(models.Model):
-    caption=models.CharField(max_length=100)
-    video=models.FileField(upload_to="videoUpload/%y")
-    def __str__(self):
-        return self.caption
+    video = models.FileField(upload_to="refVideoUpload")
